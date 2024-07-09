1aca6fa4d98bda7ccdcad4d0fd76587a5e1ec0cf
Katherine Muñoz <katy.miyu24@gmail.com>
2024-07-09 00:53:52 -0400

    Navbar y modelo y view de Navbar

--- usuarios/models.py
+++ usuarios/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Nav_home(models.Model):
-			id_nav_home = models.AutoField(db_column="idNavhome", primary_key=True)
-			nav_home = models.CharField(max_length=20, blank=False, null=False)
-
-			def __str__(self):
-				return str(self.nav_home)
+	id_nav_home = models.AutoField(db_column="idNavhome", primary_key=True)
+	nav_home = models.CharField(max_length=20, blank=False, null=False)
+	url_home = models.CharField(max_length=100, default='default_value')
+	def __str__(self):
+		return str(self.nav_home)
 			
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 
 def home(request):
-			context = {}
-			return render (request, 'usuarios/home.html', context) 
+	navs_home = Nav_home.objects.all()
+	context = {
+		'navs_home':navs_home
+	}
+	return render (request, 'usuarios/home.html', context) 
 
 def login(request):
-			context = {}
-			return render (request, 'usuarios/login.html', context)
+	navs_home = Nav_home.objects.all()
+	
+	context = {
+		'navs_home':navs_home
+	}
+	return render (request, 'usuarios/login.html', context)
+
+
 def local(request):
-			context = {}
-			return render (request, 'usuarios/local.html', context)
+	navs_home = Nav_home.objects.all()
+	context = {
+		'navs_home':navs_home
+	}
+	return render (request, 'usuarios/local.html', context)
